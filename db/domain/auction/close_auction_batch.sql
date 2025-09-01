90663a21b47bb922d400e5d3cac718643732a91d
zkvn1103@naver.com <zkvn1103@naver.com>
2025-09-01 19:50:55 +0900

    fix : 낙찰 논리 오류 수정

--- db/domain/auction/close_auction_batch.sql
+++ db/domain/auction/close_auction_batch.sql
@@ -12,7 +12,10 @@ BEGIN
         SELECT auction_id
           FROM auction_item
          WHERE auction_end_time <= NOW(6)
-           AND bid_status = 'B'
+            AND (
+			       bid_status = 'B'
+			    OR bid_status IS NULL
+			)
          ORDER BY auction_end_time ASC;
 
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
@@ -25,7 +28,6 @@ BEGIN
         END IF;
 
         CALL sp_close_one_auction(v_id);
-        -- 실패해도 sp_close_one_auction 내부에서 롤백/RESIGNAL 하므로 여기선 다음으로 진행
     END LOOP;
     CLOSE cur;
 END;
