6fd25f623aaa414509a3cbb1c7b677df1c2762dc
Jnkgb <pjw0918jw@naver.com>
2025-09-03 09:40:21 +0900

    prefix: 문의글 조회 성능 개선 인덱스 추가

--- db/init/create_idx.sql
+++ db/init/create_idx.sql
@@ -5,3 +5,7 @@ ON auction_item (auction_end_time, bid_status, auction_id);
 -- 거래내역 조회 성능 개선 인덱스
 CREATE INDEX idx_ab_created_at
 ON auction_bid (created_at, auction_id, user_id, bid_amount, success_status);
+
+-- 문의글 조회 성능 개선 인덱스
+CREATE INDEX idx_inquiry_answer_status 
+ON inquiry (inquiry_status);
